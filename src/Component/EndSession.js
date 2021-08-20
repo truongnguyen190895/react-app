ec77e88c3535196ba33dea958d0b12fc9da678f5
binhboong <babinh.1602@gmail.com>
2021-08-20 08:53:42 +0700

    create_component

---
+++ src/Component/EndSession.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+EndSession.propTypes = {};
+
+function EndSession(props) {
+  return <div></div>;
+}
+
+export default EndSession;
