702b1ce01c78d716536737b340b60fb67b05134b
binhboong <babinh.1602@gmail.com>
2021-08-31 09:20:43 +0700

    update_firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyCBZ2K3BdmfCw41az9shRSYiJV6atOjyVw",
-  authDomain: "chat-app-e7f44.firebaseapp.com",
-  projectId: "chat-app-e7f44",
-  storageBucket: "chat-app-e7f44.appspot.com",
-  messagingSenderId: "260404544437",
-  appId: "1:260404544437:web:15dce4159cf57d42c0cffc",
-  measurementId: "G-S2B5DT669E",
+  apiKey: "AIzaSyBVsXNzETD01pKp0sgqrM6GuZz-XdXg5Yo",
+  authDomain: "react-app-3da57.firebaseapp.com",
+  projectId: "react-app-3da57",
+  storageBucket: "react-app-3da57.appspot.com",
+  messagingSenderId: "1065230059741",
+  appId: "1:1065230059741:web:8f809c54f9a8d8c064b334",
+  measurementId: "G-4YVHM1PWSJ",
 });
 
 const db = app.firestore();
